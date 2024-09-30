f276e9a8775a23f5a22b1bbf2e9a9352e31d26c4
marinhotg <tiago.mlima@hotmail.com>
2024-09-30 15:17:51 -0300

    db seed

--- actions/actions.ts
+++ actions/actions.ts
@@ -4,15 +4,19 @@ import prisma from "@/lib/db";
 import { revalidatePath } from "next/cache";
 
 export async function createUser(formData: FormData) {
-  await prisma.user.create({
-    data: {
-			name: formData.get("name") as string,
-			email: formData.get("email") as string,
-			password: formData.get("password") as string,
-			status: formData.get("status") as string,
-			role: formData.get("role") as string,
-		}
-	});
+	try {
+		await prisma.user.create({
+			data: {
+				name: formData.get("name") as string,
+				email: formData.get("email") as string,
+				password: formData.get("password") as string,
+				status: formData.get("status") as string,
+				role: formData.get("role") as string,
+			}
+		});
+	} catch (error) {
+		console.log(error);
+	}
 
 	revalidatePath("/users");
 };
