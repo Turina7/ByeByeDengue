0ff32945455f5976f186606acb1c990a72706ad1
marinhotg <tiago.mlima@hotmail.com>
2024-10-03 11:05:19 -0300

    remove duplicate tabMenu

--- app/(pages)/forum/page.tsx
+++ app/(pages)/forum/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { FC, useState } from "react";
 import Head from "next/head";
 import styles from "@/app/page.module.css";
-import TabMenu from "@/app/components/tabMenu";
 
 interface PageProps {
   title: string;
@@ -11,14 +9,6 @@ interface PageProps {
 }
 
 const Page: FC<PageProps> = ({ title, content }) => {
-  // Define the activeTab state
-  const [activeTab, setActiveTab] = useState<string>("Fórum");
-
-  // Handler to change the active tab
-  const handleTabChange = (tab: string) => {
-    setActiveTab(tab);
-  };
-
   return (
     <>
       <Head>
@@ -30,7 +20,6 @@ const Page: FC<PageProps> = ({ title, content }) => {
       <main className={styles.main}>
         <section className={styles.section}>
           <h1 className={styles.title}>{title}</h1>
-          <TabMenu activeTab={activeTab} onTabChange={handleTabChange} />
           <h1>Fórum</h1>
           <p className={styles.content}>{content}</p>
         </section>
