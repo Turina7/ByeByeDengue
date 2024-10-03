3eeb8a5778f5b3f893b841825521ae5860387b0c
marinhotg <tiago.mlima@hotmail.com>
2024-10-03 10:42:30 -0300

    client layout to global tab menu

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,11 @@
 "use client";
 
-import TabMenu from "@/app/components/tabMenu"; // Import TabMenu component
-import { useState } from "react";
-import styles from "./page.module.css"; // Import CSS
-import Button from "./components/button";
+import styles from "./page.module.css"; 
 
 export default function Home() {
-  const [activeTab, setActiveTab] = useState("Home"); // Initial active tab
-
-  // Function to change the active tab
-  const handleTabChange = (tab: string) => {
-    setActiveTab(tab);
-  };
-
   return (
     <div className={styles.ctas}>
       <main className={styles.main}>
-
-        {/* TabMenu component, receives the active tab and the tab change function */}
-        <TabMenu activeTab={activeTab} onTabChange={handleTabChange} />
-
-        {/* Dynamic content based on the active tab */}
         <div style={{ padding: "20px" }}>
           <h1>Home Page</h1>
         </div>
