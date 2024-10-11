4c1d3a955ec04c308035c7e2bb26bc3779704683
marinhotg <tiago.mlima@hotmail.com>
2024-10-11 12:02:51 -0300

    homepage sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,24 @@
 "use client";
 
 import styles from './page.module.css';
+import Link from "next/link";
 import Image from "next/image"; 
 import vetor from "@/app/images/Vetor.jpg";
 import symptoms from "@/app/images/Sintomas.png";
 import prevention from "@/app/images/Prevencao.jpeg";
+import mosquito from "@/app/images/mosquito.jpg";
+import hospital from "@/app/images/hospital.png";
+import meme1 from "@/app/images/meme1.jpg";
+import meme2 from "@/app/images/meme2.png";
+import mosquitoImage from "@/app/images/mosquito.jpg";
+import campaignImage from "@/app/images/campaign.jpg";
+import willSmithImage from "@/app/images/will-smith.jpg";
+import drDrauzioImage from "@/app/images/dr-drauzio.jpg";
+import bobMarleyImage from "@/app/images/bob-marley.jpg";
 
 export default function Home() {
   return (
+    //TODO: sections to be dynamic
     <div className={styles.container}>
       <main className={styles.main}>
         <div className={styles.intro}>
@@ -62,6 +73,92 @@ export default function Home() {
             </div>
           </div>
         </div>
+        
+        <div className={styles.news}>
+          <h2>Notícias</h2>
+          <div className={styles.newsItem}>
+            <Image src={mosquito} alt="Mosquito"/>
+            <div>
+              <h3>Study identifies areas of Europe at risk from dengue fever due to spread of Asian tiger mosquito</h3>
+              <p>As Europe grapples with the growing threat of tropical diseases brought by the Asian tiger mosquito...</p>
+            </div>
+          </div>
+          <div className={styles.newsItem}>
+            <Image src={hospital} alt="Hospital"/>
+            <div>
+              <h3>Dengue: 5 more die, 534 hospitalized in 24hrs</h3>
+              <p>Five more deaths were reported from dengue in 24 hours till Tuesday morning, raising the number of fatalities from the...</p>
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.communityPosts}>
+          <h2>Posts da comunidade</h2>
+          <div className={styles.postsContainer}>
+            <div className={styles.postItem}>
+              <Image src={meme1} alt="Meme 1"/>
+            </div>
+            <div className={styles.postItem}>
+              <Image src={meme2} alt="Meme 2"/>
+            </div>  
+          </div>
+          <div className={styles.buttonContainer}>
+            <Link href = "/forum">
+              <button className={styles.forumButton}>Acesse o Fórum</button>
+            </Link>
+          </div>
+        </div>
+        <section className={styles.explore}>
+          <h2>Explore</h2>
+          <div className={styles.exploreGrid}>
+            <div className={styles.exploreItem}>
+              <Image src={mosquitoImage} alt="Mosquito"/>
+              <h3>Artigos e pesquisas</h3>
+              <p>Lorem ipsum dolor amet, consectetuer adipiscing elit. Sed netus volutpat vitae curae finibus tortor enim venenatis.</p>
+            </div>
+            <div className={styles.exploreItem}>
+              <Image src={campaignImage} alt="Campanha de combate"/>
+              <h3>Apoie as instituições de combate</h3>
+              <p>Lorem ipsum dolor amet, consectetuer adipiscing elit. Sed netus volutpat vitae curae finibus tortor enim venenatis.</p>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.feedbacks}>
+          <h2>Feedbacks</h2>
+          <div className={styles.feedbackGrid}>
+            <div className={styles.feedbackItem}>
+              <p>Site muito bom!</p>
+              <div className={styles.feedbackAuthor}>
+                <Image src={willSmithImage} alt="Will Smith" width={50} height={50} />
+                <div>
+                  <h4>Will Smith</h4>
+                  <p>Dengue Specialist</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.feedbackItem}>
+              <p>Comunidade muito engajada</p>
+              <div className={styles.feedbackAuthor}>
+                <Image src={drDrauzioImage} alt="Dr. Drauzio Varella" width={50} height={50} />
+                <div>
+                  <h4>Dr. Drauzio Varella</h4>
+                  <p>Doctor</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.feedbackItem}>
+              <p>Dengue nunca mais!</p>
+              <div className={styles.feedbackAuthor}>
+                <Image src={bobMarleyImage} alt="Bob Marley" width={50} height={50} />
+                <div>
+                  <h4>Bob Marley</h4>
+                  <p>Scientist</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </main>
     </div>
   );
