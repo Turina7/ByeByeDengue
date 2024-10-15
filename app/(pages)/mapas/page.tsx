a402d6e7f137dd134acef83a5041101c20612b48
jimp4s <jpgarcia278@usp.br>
2024-10-15 11:55:31 -0300

    Mock das paginas Math e Maps

--- app/(pages)/mapas/page.tsx
+++ app/(pages)/mapas/page.tsx
@@ -1,31 +1,41 @@
-"use client";
-
-import Head from "next/head";
-import styles from "@/app/page.module.css";
+import React from 'react';
+import Head from 'next/head';
+import styles from '.@/app/page.module.css';
 
 interface PageProps {
   title: string;
-  content: string;
+  locationInfo: string;
 }
 
-const Page: FC<PageProps> = ({ title, content }) => {
+const MapPage: React.FC<PageProps> = ({ title, locationInfo }) => {
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content="Generic page layout" />
+        <meta name="description" content="Página com mapa interativo" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <main className={styles.main}>
-        <section className={styles.section}>
+        <section>
           <h1 className={styles.title}>{title}</h1>
-          <h1>Mapas</h1>
-          <p className={styles.content}>{content}</p>
+          <br/>
+          <div className={styles.mapContainer}>
+            {/* Espaço para o mapa mockado */}
+            <div className={styles.map}>
+              <img src="/image/mapas.jpg" alt="Mapa Interativo" className={styles.mapImage} />
+            </div>
+
+            {/* Informações do local ao lado do mapa */}
+            <div className={styles.locationInfo}>
+              <h2>Informações do Local</h2>
+              <p>{locationInfo}</p>
+            </div>
+          </div>
         </section>
       </main>
     </>
   );
 };
 
-export default Page;
+export default MapPage;
