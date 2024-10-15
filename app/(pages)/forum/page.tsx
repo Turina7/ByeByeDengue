3076152d07b2b51305600fc1178ae743499262bd
giovani <giovani.seb@usp.br>
2024-10-15 17:16:20 -0300

    Adição da base da seção Fórum

--- app/(pages)/forum/page.tsx
+++ app/(pages)/forum/page.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import Head from "next/head";
+import style from "@/app/components/forumCard/forumCard.module.css";
 import styles from "@/app/page.module.css";
+import ForumCard, { cardContent } from "@/app/components/forumCard/forumCard";
+import { FC } from "react";
 
 interface PageProps {
   title: string;
@@ -9,6 +12,34 @@ interface PageProps {
 }
 
 const Page: FC<PageProps> = ({ title, content }) => {
+
+  // variável que armazenará postagens consultadas no banco
+  const forumPosts: cardContent[] = [
+    {
+      header: "Tiago Marinho repostou:",
+      post: "O Ministério da Saúde adverte: se seque bem após o banho, água parada em pneus dá dengue.",
+      message: "O que banho tem a ver com pneu?",
+      comments: [],
+    },
+    {
+      header: "João Paulo comentou:",
+      post: "Por favor, lembrem-se de não acumular água parada nos quintais.",
+      message: "Muito importante, João!",
+      comments: ["Falou e disse.","Pois é!", "Tem razão, João"],
+    },
+    {
+      header: "Guilherme repostou:",
+      post: "Prestem atenção nos focos do mosquito!",
+      message: "Concordo!",
+      comments: ["Verdade, todos temos que colaborar."],
+    },
+  ];
+
+  const handleComment = () => {
+    console.log('Comentar');
+    // tratar para criar comentário
+  }
+
   return (
     <>
       <Head>
@@ -21,7 +52,18 @@ const Page: FC<PageProps> = ({ title, content }) => {
         <section className={styles.section}>
           <h1 className={styles.title}>{title}</h1>
           <h1>Fórum</h1>
-          <p className={styles.content}>{content}</p>
+          <p>Este é o espaço da comunidade!
+            Aqui você pode ver as postagens em alta e publicar sua mensagem ou comentário. Experimente!</p>
+          <div className={style.cardContainer}>
+            {forumPosts.map((post, index) => (
+              <ForumCard
+                key={index}
+                cardContent={post}
+                active={true}
+                handleComment={handleComment}
+              />
+            ))}
+          </div>
         </section>
       </main>
     </>
