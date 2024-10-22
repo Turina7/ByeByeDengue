3baf04935e7b97a7f42d307126709f9ba3cc2651
marinhotg <tiago.mlima@hotmail.com>
2024-10-22 19:46:48 -0300

    Revert "Merge branch 'report-front'"
    This reverts commit 5a5ab7d1beb0baf3d146f10510f5ec575a7310dc, reversing
    changes made to c508f00842f253ece498238727693c4751ecbad4.

--- app/(pages)/mapas/page.tsx
+++ app/(pages)/mapas/page.tsx
@@ -1,43 +1,31 @@
-import React from 'react';
-import Head from 'next/head';
-import styles from './mapas.module.css';
-import Image from 'next/image';
-import map from '@/app/images/mapas.png';
+"use client";
+
+import Head from "next/head";
+import styles from "@/app/page.module.css";
 
 interface PageProps {
   title: string;
-  locationInfo: string;
+  content: string;
 }
 
-const MapPage: React.FC<PageProps> = ({ title, locationInfo }) => {
+const Page: FC<PageProps> = ({ title, content }) => {
   return (
     <>
       <Head>
         <title>{title}</title>
-        <meta name="description" content="Página com mapa interativo" />
+        <meta name="description" content="Generic page layout" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
       <main className={styles.main}>
-        <section>
+        <section className={styles.section}>
           <h1 className={styles.title}>{title}</h1>
-          <br/>
-          <div className={styles.mapContainer}>
-            {/* Espaço para o mapa mockado */}
-            <div className={styles.map}>
-              <Image src={map} alt="Mapa Interativo" className={styles.mapImage} />
-            </div>
-
-            {/* Informações do local ao lado do mapa */}
-            <div className={styles.locationInfo}>
-              <h2>Informações do Local</h2>
-              <p>{locationInfo}</p>
-            </div>
-          </div>
+          <h1>Mapas</h1>
+          <p className={styles.content}>{content}</p>
         </section>
       </main>
     </>
   );
 };
 
-export default MapPage;
+export default Page;
