33e67d51b06700164bfd40a9138b8faf04517371
giovani <giovani.seb@usp.br>
2024-11-19 11:46:01 -0300

    Adição da página de configurações de usuário

--- middleware.ts
+++ middleware.ts
@@ -26,5 +26,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/forum/:path*', '/denuncias/:path*'],
+  matcher: ['/forum/:path*', '/denuncias/:path*', '/settings/:path*'],
 };
