07a2b0b16b27da27df9ef3c643cfde1e568faab2
marinhotg <tiago.mlima@hotmail.com>
2024-11-27 13:03:09 -0300

    moving article details to a page

--- app/components/wikipageSections/ArticleComponent.tsx
+++ app/components/wikipageSections/ArticleComponent.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
 import styles from '@/app/components/wikipageSections/ArticleComponent.module.css';
-import { Modal, Box, Typography} from '@mui/material';
-import Button from '@/app/components/button/button'
+import {  Typography} from '@mui/material';
+import Link from 'next/link';
 
 interface ArticleComponentProps {
   title: string;
@@ -12,50 +11,21 @@ interface ArticleComponentProps {
 }
 
 const ArticleComponent: React.FC<ArticleComponentProps> = ({
+  id,
   title,
   author,
   date,
-  text,
   summary,
 }) => {
-  const [open, setOpen] = useState(false);
 
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   return (
-    <div>
-      {/* Display main content */}
-      <div onClick={handleOpen} className={styles.artigoContainer}>
-        <Typography variant="h5" className={styles.title}>{title}</Typography>
-        <Typography variant="subtitle1" className={styles.authorDate}>By {author}</Typography>
-        <Typography variant="caption" className={styles.authorDate}>{date}</Typography>
-        <Typography variant="body2" className={styles.summary}>{summary}</Typography>
-      </div>
-
-      {/* Centered modal with margins */}
-      <Modal open={open} onClose={handleClose} aria-labelledby="article-modal-title">
-        <Box className={styles.modalContainer}>
-          <Box className={styles.a4Format}>
-            <Typography id="article-modal-title" variant="h4" gutterBottom>
-              {title}
-            </Typography>
-            <Typography variant="subtitle1">By {author}</Typography>
-            <Typography variant="caption">{date}</Typography>
-            <Typography variant="body2" sx={{ mt: 2 }}>
-              {summary}
-            </Typography>
-            <Typography variant="body1" sx={{ mt: 2 }}>
-              {text}
-            </Typography>
-            <br/>
-            <Button onClick={handleClose}>
-              Close
-            </Button>
-          </Box>
-        </Box>
-      </Modal>
-    </div>
+    <Link href={`/wiki/${id}`} className={styles.artigoContainer}>
+      <Typography variant="h5" className={styles.title}>{title}</Typography>
+      <Typography variant="subtitle1" className={styles.authorDate}>By {author}</Typography>
+      <Typography variant="caption" className={styles.authorDate}>{date}</Typography>
+      <Typography variant="body2" className={styles.summary}>{summary}</Typography>
+    </Link>
   );
 };
 
